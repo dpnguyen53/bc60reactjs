b89d08e13a001138ea04b372d69a313e2db6b214
dpnguyen53 <dpnguyen53@gmail.com>
2024-01-12 19:17:19 +0700

    Buoi 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
 // import Baitap1 from "./baitap1";
 // import Baitap2 from "./baitap2";
-import Baitap3 from "./baitap3";
+// import Baitap3 from "./baitap3";
+import RenderingElements from "./rendering-elements";
+import HandlingEvents from "./handling-events";
+import State from "./state";
 
 function App() {
   return (
-    <div>
+    <>
       {/* <Baitap1 /> */}
       {/* <Baitap2 /> */}
-      <Baitap3 />
-    </div>
+      {/* <Baitap3 /> */}
+      <RenderingElements />
+      <hr />
+      <HandlingEvents />
+      <hr />
+      <State />
+    </>
   );
 }
 
---
+++ src/state/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class State extends Component {
+  render() {
+    return (
+      <div>
+        <h3>* State</h3>
+      </div>
+    );
+  }
+}
